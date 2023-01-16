c3894ea4b2c0d134d5797195e8a19945ff4cbb39
Bensas <juanbensa@gmail.com>
2023-01-16 17:49:22 -0300

    Added ability to load program from hex string, included button to load sample program

--- src/main.rs
+++ src/main.rs
@@ -13,9 +13,23 @@ struct CPUVisualizer {
   cpu: Ben6502
 }
 
+impl CPUVisualizer {
+  fn load_program_to_ram(&mut self, program_str: &str, start_addr: u16) {
+    let hex_strings = program_str.split(" ");
+
+    let mut curr_addr = start_addr;
+    for hex_str in hex_strings.into_iter() {
+      let value: u8 = u8::from_str_radix(hex_str, 16).unwrap();
+      self.cpu.bus.write(curr_addr, value).unwrap();
+      curr_addr += 1;
+    }
+  }
+}
+
 #[derive(Debug, Clone, Copy)]
 enum Message {
   NextInstruction,
+  LoadProgram
 }
 
 impl Sandbox for CPUVisualizer {
@@ -23,14 +37,9 @@ impl Sandbox for CPUVisualizer {
 
   fn new() -> Self {
     let mut mem_bus = Bus16Bit::new();
-    mem_bus.write(0xFFFC, 0x00).unwrap();
-    mem_bus.write(0xFFFD, 0x80).unwrap();
-
-    mem_bus.write(0x8000, 0x38).unwrap();
-
-
+    mem_bus.write(emulation::PROGRAM_START_POINTER_ADDR, 0x00).unwrap();
+    mem_bus.write(emulation::PROGRAM_START_POINTER_ADDR + 1, 0x80).unwrap();
     let cpu: Ben6502 = Ben6502::new(mem_bus);
-
     Self { 
       cpu: cpu
     }
@@ -44,7 +53,10 @@ impl Sandbox for CPUVisualizer {
       match message {
           Message::NextInstruction => {
               self.cpu.clock_cycle();
-          }
+          },
+          Message::LoadProgram => {
+            self.load_program_to_ram("A2 0A 8E 00 00 A2 03 8E 01 00 AC 00 00 A9 00 18 6D 01 00 88 D0 FA 8D 02 00 EA EA EA", 0x8000);
+        }
       }
   }
 
@@ -55,11 +67,14 @@ impl Sandbox for CPUVisualizer {
     let stack_start_addr = 0x100 + emulation::SP_RESET_ADDR as u16 - 100;
     let stack_end_addr = 0x100 + emulation::SP_RESET_ADDR as u16;
       column![
+          text(format!("RAM contents (Addr 0x{:x} - 0x{:x}):", 0x00, 0x50)),
+          text(self.cpu.bus.get_memory_content_as_string(0x00, 0x50)).size(20),
           text(format!("RAM contents (Addr 0x{:x} - 0x{:x}):", ram_start_addr, ram_end_addr)),
           text(self.cpu.bus.get_memory_content_as_string(ram_start_addr, ram_end_addr)).size(20),
           text(format!("Stack contents (Addr 0x{:x} - 0x{:x}):", stack_start_addr, stack_end_addr)),
           text(self.cpu.bus.get_memory_content_as_string(stack_start_addr, stack_end_addr)).size(20),
           button("Next Clock Cycle").on_press(Message::NextInstruction),
+          button("Load Program").on_press(Message::LoadProgram),
           row![
             column![
               text("Cpu registers:").size(20),
